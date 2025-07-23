d6d5b442a323b8f42cfdaa8035406524cb37ff6e
DiegoFreo <diego_freo8@hotmail.com>
2025-07-23 08:31:26 -0300

    Subindo page

---
+++ src/app/(oauth)/cronometrista/consulta/prova.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ConsultaProva = () => {
+  return (
+    <div>
+      <h1>Consulta de Prova</h1>
+      <p>Esta é a página de consulta de prova.</p>
+    </div>
+  );
+}
+export default ConsultaProva;
