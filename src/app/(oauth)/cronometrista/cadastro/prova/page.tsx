a7f4c7f7824737e9a4c241add94248b12a98c0ea
DiegoFreo <diego_freo8@hotmail.com>
2025-07-23 08:21:10 -0300

    Subindo page

---
+++ src/app/(oauth)/cronometrista/cadastro/prova/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CadProva = () => {
+  return (
+    <div>
+      <h1>Cadastro de Prova</h1>
+      <p>Esta é a página de cadastro de prova.</p>
+    </div>
+  );
+}
+
+export default CadProva;
