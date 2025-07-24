48612281ad02c38e6795ad08466f477b280552f6
DiegoFreo <diego_freo8@hotmail.com>
2025-07-24 19:42:11 -0300

    Atualizando paginas

--- src/app/(oauth)/admin/page.tsx
+++ src/app/(oauth)/admin/page.tsx
@@ -1,21 +1,84 @@
 import React from "react";
+import { Card, CardContent } from "@/componets/ui/card";
+import Button from "@/componets/ui/Buttom";
+import {UserPen, User, Trophy, FolderTree, MapPin} from "lucide-react";
+import '../../../componets/dashboard.css';
 
 
 export default function AdminPage() {
   return (
-    <div className="content-center content-gap-20 flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="box-sign-in p-20">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm img-center">
-          <img
-            alt="logo"
-            src="./logoka.svg"
-            className="mx-auto h-30 w-auto"
-          />
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Bem-vindo à página de administração
-          </h2>
+      <div className="continerdashboard">
+        <div className="continerdashboard-left">
+          <div className="continerdashboard-logo pb-4">
+            <img
+              alt="logo"
+              src="./logoka.svg"
+              className="mx-auto h-15 w-auto"
+            />
+            <h2 >
+              CRONOKA
+            </h2>
+          </div>
+          <div className="continerdashboard-menu pt-4">
+            <h2>MENU</h2>
+            <ul>
+              <li>Competidores</li>
+              <li>Usuário</li>
+              <li>Categoria</li>
+              <li>Prova</li>
+              <li>Eventos</li>
+            </ul>
+
+          </div>
+          <div className="continerdashboard-logout pt-4">
+            <div className="continerdashboard-logout-perfill">
+              <Button className="bg-cronometro btn-corrida">Sair</Button>
+              <img
+                alt="perfil"
+                src="./logoka.svg"
+                className="mx-auto h-15 w-auto"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="continerdashboard-right">
+          <Card className="w-50 h-full">
+            <CardContent className="flex flex-col items-center justify-center">
+              <h2 className="text-2xl font-bold mb-4">Usuário</h2>
+              <UserPen className="w-20 h-20 mb-2 color-cronometro-pause" />
+              <Button className="btn-corrida bg-cronometro" >Incluir</Button>
+            </CardContent>
+          </Card>
+          <Card className="w-50 h-full">
+            <CardContent className="flex flex-col items-center justify-center">
+              <h2 className="text-2xl font-bold mb-4">Competidor</h2>
+              <User className="w-20 h-20 mb-2 color-cronometro-pause" />
+              <Button className="btn-corrida bg-cronometro" >Incluir</Button>
+            </CardContent>
+          </Card>
+          <Card className="w-50 h-full">
+            <CardContent className="flex flex-col items-center justify-center">
+              <h2 className="text-2xl font-bold mb-4">Eventos</h2>
+              <Trophy className="w-20 h-20 mb-2 color-cronometro-pause" />
+              <Button className="btn-corrida bg-cronometro" >Incluir</Button>
+            </CardContent>
+          </Card>
+           <Card className="w-50 h-full">
+            <CardContent className="flex flex-col items-center justify-center">
+              <h2 className="text-2xl font-bold mb-4">Categoria</h2>
+              <FolderTree className="w-20 h-20 mb-2 color-cronometro-pause" />
+              <Button className="btn-corrida bg-cronometro" >Incluir</Button>
+            </CardContent>
+          </Card>
+          <Card className="w-50 h-full">
+            <CardContent className="flex flex-col items-center justify-center">
+              <h2 className="text-2xl font-bold mb-4">Bateria</h2>
+              <MapPin className="w-20 h-20 mb-2 color-cronometro-pause" />
+              <Button className="btn-corrida bg-cronometro" >Incluir</Button>
+            </CardContent>
+          </Card>
+
         </div>
       </div>
-    </div>
   );
 }
