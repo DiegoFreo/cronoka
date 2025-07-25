ed7e921071db75d96beee6f21329f077d1ecce58
DiegoFreo <diego_freo8@hotmail.com>
2025-07-24 21:55:02 -0300

    Subindo

--- src/app/(oauth)/admin/page.tsx
+++ src/app/(oauth)/admin/page.tsx
@@ -1,11 +1,65 @@
-import React from "react";
+'use client';
+import React, {useRef}  from "react";
 import { Card, CardContent } from "@/componets/ui/card";
 import Button from "@/componets/ui/Buttom";
 import {UserPen, User, Trophy, FolderTree, MapPin} from "lucide-react";
 import '../../../componets/dashboard.css';
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import {Bar}from "react-chartjs-2";
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const labels = ['Janeiro', 'Fevereiro', 'Março', 'Abril'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Eventos',
+      data: [5, 3, 3, 5],
+      backgroundColor: 'rgba(75, 192, 192, 0.6)',
+    },
+    {
+      label: 'Competidores',
+      data: [30, 20, 10, 15],
+      backgroundColor: 'rgba(153, 102, 255, 0.6)',
+    },
+  ],
+};
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Eventos e Competidores por Mês',
+    },
+  }
+};
+
 
 export default function AdminPage() {
+  
   return (
       <div className="continerdashboard">
         <div className="continerdashboard-left">
@@ -35,7 +89,7 @@ export default function AdminPage() {
               <Button className="bg-cronometro btn-corrida">Sair</Button>
               <img
                 alt="perfil"
-                src="./logoka.svg"
+                src="https://github.com/jmarioasilva.png"
                 className="mx-auto h-15 w-auto"
               />
             </div>
@@ -75,6 +129,13 @@ export default function AdminPage() {
               <h2 className="text-2xl font-bold mb-4">Bateria</h2>
               <MapPin className="w-20 h-20 mb-2 color-cronometro-pause" />
               <Button className="btn-corrida bg-cronometro" >Incluir</Button>
+            </CardContent>
+          </Card>
+          <Card className="w-full h-full">
+            <CardContent className="flex flex-col items-center justify-center">
+              <div className="w-250 h-100">
+                <Bar  data={data} options={options}/>
+              </div>
             </CardContent>
           </Card>
 
