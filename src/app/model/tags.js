a760c110a06eede8174d51ae581ef45e6d86cb97
DiegoFreo <diego_freo8@hotmail.com>
2025-11-23 23:12:08 -0300

    tabelas do banco

--- src/app/model/tags.js
+++ src/app/model/tags.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import { unique } from 'next/dist/build/utils';
 
 const TagSchema = new mongoose.Schema({
-    num: {type: String, require: true, unique: true },
     tag: { type: String, required: true, unique: true },
 }, { timestamps: true });
 export default mongoose.models.Tag || mongoose.model('Tag', TagSchema);
